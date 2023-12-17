1a75823af721d082988819acee2a0f29371e56fe
thanushayan <thanushayan17@gmail.com>
2023-12-17 18:35:53 +0530

    deploy

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaLocationArrow,
+  FaMobileAlt,
+} from "react-icons/fa";
+import footerLogo from "../../assets/food-logo.png";
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-100 dark:bg-gray-950">
+      <section className="max-w-[1200px] mx-auto">
+        <div className=" grid md:grid-cols-3 py-5">
+          <div className=" py-8 px-4 ">
+            <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3">
+              <img src={footerLogo} alt="Logo" className="max-w-[50px]" />
+              FOODIE
+            </h1>
+            <p className="">
+              Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Possimus, voluptate.{" "}
+            </p>
+            <br />
+            <div className="flex items-center gap-3">
+              <FaLocationArrow />
+              <p>Nelukulam, Vavuniya 
+
+              </p>
+            </div>
+            <div className="flex items-center gap-3 mt-3">
+              <FaMobileAlt />
+              <p>+94 776408775</p>
+            </div>
+            {/* Social Handle */}
+            <div className="flex items-center gap-3 mt-6">
+              <a href="https://www.instagram.com/thanu0223/">
+                <FaInstagram className="text-3xl" />
+              </a>
+              <a href="https://www.facebook.com/thanushayan98/">
+                <FaFacebook className="text-3xl" />
+              </a>
+              <a href="https://www.linkedin.com/in/suthagar-thanushayan-a50212183/">
+                <FaLinkedin className="text-3xl" />
+              </a>
+            </div>
+          </div>
+          <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10 ">
+            <div className="">
+              <div className="py-8 px-4 ">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Important Links
+                </h1>
+                <ul className={`flex flex-col gap-3`}>
+                  <li className="cursor-pointer">Home</li>
+                  <li className="cursor-pointer">About</li>
+                  <li className="cursor-pointer">Services</li>
+                  <li className="cursor-pointer">Login</li>
+                </ul>
+              </div>
+            </div>
+            <div className="">
+              <div className="py-8 px-4 ">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Links
+                </h1>
+                <ul className="flex flex-col gap-3">
+                  <li className="cursor-pointer">Home</li>
+                  <li className="cursor-pointer">About</li>
+                  <li className="cursor-pointer">Services</li>
+                  <li className="cursor-pointer">Login</li>
+                </ul>
+              </div>
+            </div>
+            <div className="">
+              <div className="py-8 px-4 ">
+                <h1 className="sm:text-xl text-xl font-bold sm:text-left text-justify mb-3">
+                  Links
+                </h1>
+                {/* <ul className="list-disc list-inside"> */}
+                <ul className="flex flex-col gap-3">
+                  <li className="cursor-pointer">Home</li>
+                  <li className="cursor-pointer">About</li>
+                  <li className="cursor-pointer">Services</li>
+                  <li className="cursor-pointer">Login</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className="text-center py-10 border-t-2 border-gray-300/50">
+            @copyright 2024 All rights reserved || Made with ❤️ by Thanushayan
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Footer;
